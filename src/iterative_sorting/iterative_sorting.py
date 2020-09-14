8cfe4c592a8af12b94a37c243863b6eaad6402d4
Chris King <cking5836@gmail.com>
2020-09-14 13:33:41 -0400

    Added comments for future reference

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,14 +1,18 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(len(arr) - 1):
+        # keep track of our current index
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
+        # start at curr_index + 1
         for j in range(i + 1, len(arr)):
+            # if current index from prev loop is greater than index at current loop
             if arr[smallest_index] > arr[j]:
+                # make index from current loop smallest_index
                 smallest_index = j
 
         # TO-DO: swap
@@ -33,9 +37,13 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     n = len(arr)
+    # loop through length of array
     for i in range(n - 1):
-        for j in range(0, n - i - 1):
+        # loop through length of array starting from index from prev loop
+        for j in range(n - i - 1):
+            # if current index is smallest than the next
             if arr[j] > arr[j + 1]:
+                # swap
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
+    # loop through the length of arr
     for i in range(len(arr)):
+        # if the index we're currently at is our target, great! if not, continue
         if arr[i] == target:
             return i
     return -1  # not found
@@ -12,12 +14,18 @@ def binary_search(arr, target):
     low = 0
     high = len(arr) - 1
     while low <= high:
-        mid = (high + low) // 2
+        # let mid be the length of array div by 2
+        mid = (low + high) // 2
+        # if the middle index is smaller than our target value
         if arr[mid] < target:
+            # add one to middle index
             low = mid + 1
+            # if our middle index if greater than our target value
         elif arr[mid] > target:
+            # subtract one from middle index
             high = mid - 1
         else:
+            # Return our value once found
             return mid
 
     return -1  # not found
