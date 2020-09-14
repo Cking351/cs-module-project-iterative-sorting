82571a40e6cd6c92456c187e17dbe5836480e473
Chris King <cking5836@gmail.com>
2020-09-14 12:25:25 -0400

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,16 +1,23 @@
 def linear_search(arr, target):
     # Your code here
-    for i in arr:
-        if i == target:
-            return print(i)
-    return print(-1)  # not found
-# this function is correct. Test says it returns 1, but below it returns a -1
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1  # not found
 
-arr1 = [-2, 7, 3, -9, 5, 1, 0, 4, -6]
-linear_search(arr1, 6)
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
     # Your code here
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < target:
+            low = mid + 1
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            return mid
 
     return -1  # not found
