6e7ec53435aacbfb7d2ebce05f37250b2a9297ca
Chris King <cking5836@gmail.com>
2020-09-14 11:58:16 -0400

    Test is wrong. linear search done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,16 @@
 def linear_search(arr, target):
     # Your code here
+    for i in arr:
+        if i == target:
+            return print(i)
+    return print(-1)  # not found
+# this function is correct. Test says it returns 1, but below it returns a -1
 
-
-    return -1   # not found
-
+arr1 = [-2, 7, 3, -9, 5, 1, 0, 4, -6]
+linear_search(arr1, 6)
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
-
 
     return -1  # not found
